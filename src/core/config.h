1866908a481eefe7043c2ca39872ae4bacad2403
Brett <ke9bos@pigletradio.org>
2026-09-20 14:45:18 -0400

    chore: bump version to 0.84

--- src/core/config.h
+++ src/core/config.h
@@ -9,7 +9,7 @@
 // ============================================
 // Firmware Version Information
 // ============================================
-#define FIRMWARE_VERSION "0.83"
+#define FIRMWARE_VERSION "0.84"
 #define FIRMWARE_DATE "2026-09-20"  // Update this date each time you build new firmware
 #define FIRMWARE_NAME "VAIL SUMMIT"
 #define WEB_FILES_VERSION "1.7.0"   // Expected web interface version for this firmware
